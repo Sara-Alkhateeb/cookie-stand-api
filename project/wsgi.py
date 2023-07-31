accbe06d49270a13698bc13025dcf1c1009f91e0
Sara-Alkhateeb <sarajamal11399@yahoo.com>
2023-07-31 22:24:45 +0300

    deployed linked

--- project/wsgi.py
+++ project/wsgi.py
@@ -15,5 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
 
-# vercel deployment requires "app"
+# Vercel needs variable named app
 app = application
